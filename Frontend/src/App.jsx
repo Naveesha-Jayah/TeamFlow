731269cac95326d2ecefd8ec97b0d3fd7332b0bb
Naveesha Jayah <175933977+Naveesha-Jayah@users.noreply.github.com>
2025-06-23 21:28:26 +0530

    Frontend Environment

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+         <Routes>
+
+          
+         </Routes>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
